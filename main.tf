36520fb6bf168bc3f2725d556d3f8fb068bc462a
Brewer <12806kight@gmail.com>
2024-01-06 01:14:05 -0600

    egress and ingress

--- main.tf
+++ main.tf
@@ -1,46 +1,65 @@
 resource "aws_vpc" "mtc_vpc" {
-    cidr_block = "10.123.0.0/16"
-    enable_dns_hostnames = true
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
 
-    tags = {
-        Name = "dev"
-    }
+  tags = {
+    Name = "dev"
+  }
 }
 
 resource "aws_subnet" "mtc_public_subnet" {
-  vpc_id     = aws_vpc.mtc_vpc.id
-  cidr_block = "10.123.1.0/24"
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-west-2a"
+  availability_zone       = "us-west-2a"
 
   tags = {
-        Name = "dev-public"
-    }
+    Name = "dev-public"
+  }
 }
 
 resource "aws_internet_gateway" "mtc_internet_gateway" {
-  vpc_id     = aws_vpc.mtc_vpc.id
-  
+  vpc_id = aws_vpc.mtc_vpc.id
+
   tags = {
-        Name = "dev-igw"
-    }
+    Name = "dev-igw"
+  }
 }
 
 resource "aws_route_table" "mtc_public_route" {
-  vpc_id     = aws_vpc.mtc_vpc.id
-  
+  vpc_id = aws_vpc.mtc_vpc.id
+
   tags = {
-        Name = "dev_public_route"
-    }
+    Name = "dev_public_route"
+  }
 }
 
 resource "aws_route" "default_route" {
-  route_table_id     = aws_route_table.mtc_public_route.id
+  route_table_id         = aws_route_table.mtc_public_route.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+  gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
 }
 
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.mtc_public_subnet.id
   route_table_id = aws_route_table.mtc_public_route.id
+}
+
+resource "aws_security_group" "mtg_sg" {
+  name        = "sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.mtc_vpc.id
+  ingress  {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
