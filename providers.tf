44133c3bfdb16c94bd019a29132ace1f3879d788
Brewer <12806kight@gmail.com>
2024-01-07 01:00:53 -0600

    Created a datasource

--- providers.tf
+++ providers.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "aws" {
-  region      = "us-west-2"
-  shared_config_files = ["~/.aws/config"]
+  region                   = "us-west-2"
+  shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "default"
 }
